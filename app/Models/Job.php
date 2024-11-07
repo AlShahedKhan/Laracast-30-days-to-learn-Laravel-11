4ccda32a14c00055f715481e95b9913849f42a94
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2024-11-07 17:23:05 +0600

    udpate to 11

---
+++ app/Models/Job.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Arr;
+
+class Job extends Model
+{
+    use HasFactory;
+    protected $table = 'job_listings';
+    protected $fillable = ['title','salary'];
+
+    public function employee(){
+        return $this->belongsTo(Employee::class);
+    }
+}
